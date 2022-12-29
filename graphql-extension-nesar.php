cd5d3a9575322a40bb4cf447f59d79593ba755e2
NesarAhmedRazon <grim.roy@gmail.com>
2022-12-29 14:48:38 +0600

    first commit

---
+++ graphql-extension-nesar.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Graphql Extension by Nesar
+ * Version: 0.0.1
+ * Author: Nesar Ahmed
+ * Author URI: https://github.com/NesarAhmedRazon
+ * Description: This is an extension plugin for WP-Graphql
+ * /
