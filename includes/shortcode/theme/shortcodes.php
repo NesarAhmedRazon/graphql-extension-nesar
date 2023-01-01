3612722f7c3759115423e26dcc37f19578b68051
NesarAhmedRazon <grim.roy@gmail.com>
2023-01-01 20:37:18 +0600

    ShorCode: Section class

--- includes/shortcode/theme/shortcodes.php
+++ includes/shortcode/theme/shortcodes.php
@@ -17,7 +17,7 @@ if (!class_exists('ThemeShortCodes')) {
             extract(shortcode_atts(['class' => '', 'style' => '',], $atts));
             $style = "style='" . $atts['style'] . "' \' ";
             $class = " " . $atts['class'];
-            $html = '</div></div><div class="containerSized section' . $class . '" ' . $style . '>' . do_shortcode($content) . '</div>';
+            $html = '</div></div><div class="containerSized section row' . $class . '" ' . $style . '>' . do_shortcode($content) . '</div>';
             return $html;
         }
     }
