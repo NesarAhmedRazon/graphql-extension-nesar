d77fb1143fa6c06f9fb5e68194e73314d0a859f4
NesarAhmedRazon <grim.roy@gmail.com>
2023-01-10 04:13:02 +0600

    GQL:All Custom Posttypes to GQL-v2

--- includes/gql/all-post-togql.php
+++ includes/gql/all-post-togql.php
@@ -36,3 +36,25 @@ function addPortfolioGql()
     register_post_type($slug2, $postType2);
 }
 add_action('init', 'addPortfolioGql', 100);
+
+function getAllpostTypes()
+{
+    $items = [];
+    $ignors = ['attachment', 'elementor_library', 'elementor-thhf', 'e-landing-page', 'rslide'];
+    $args = array(
+        'public'   => true,
+    );
+    $post_types = get_post_types($args, 'objects');
+
+    foreach ($ignors as $ignor) {
+        unset($post_types[$ignor]);
+    }
+    if ($post_types) {
+        foreach ($post_types  as $post_type) {
+            $k = $post_type->name;
+            $v = $post_type->label;
+            $items[$k] = esc_html__($v, 'nesar-widgets');
+        }
+        return $items;
+    }
+}
